4cc68784590255a8c929d59f1d76773c90cd5291
Troy Daniels <troyaadaniels@gmail.com>
2017-01-08 18:02:29 +1100

    Small error in preamble

--- src/implementation.php
+++ src/implementation.php
@@ -13,7 +13,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    test.php - Simple working example of lexer.php
+    implementation.php - Simple working example of lexer.php
     Troy Daniels - 08/01/17
 */
 
